0f400db60aece8db52a77d719158fc0b7f45a3da
Goncalo101 <goncalo.oli@outlook.com>
2019-05-24 12:17:46 +0100

    Changed the way the graph viewer is loaded

--- main.cpp
+++ main.cpp
@@ -41,24 +41,30 @@ vector<DeliveryPoint*> associateItems(vector<Item*>& items, Graph<Location>& gra
     return deliveries;
 }
 
+GraphViewer* init_viewer(int width, int height)
+{
+    auto gv = new GraphViewer(width, height, false);
+    gv->createWindow(width, height);
+    gv->addNode(0, 0, 0);
+
+    return gv;
+}
+
 int main(int argc, char* argv[])
 {
-    if (argc != 2) {
+    if (argc!=2) {
         cout << "Usage: " << argv[0] << " <city name>" << endl;
         exit(1);
     }
 
     string city_name(argv[1]);
     transform(city_name.begin(), city_name.end(), city_name.begin(), ::toupper);
-    transform(city_name.begin() + 1, city_name.end(), city_name.begin() + 1, ::tolower);
+    transform(city_name.begin()+1, city_name.end(), city_name.begin()+1, ::tolower);
 
-
-    auto* gv = new GraphViewer(750, 600, false);
-    gv->createWindow(750, 600);
-    gv->addNode(0, 0, 0);
+    GraphViewer* gv = init_viewer(700, 600);
 
     Location loc1 = Location(402328881);
-    Location loc2 = Location(402328911);
+    Location loc2 = Location(402328963);
 
     Graph<Location> location_graph(city_name, gv);
 
@@ -68,12 +74,12 @@ int main(int argc, char* argv[])
     location_graph.floydWarshallShortestPath();
     vector<Location> path = location_graph.getFloydWarshallPath(loc1, loc2);
 
-    if (!path.empty()){
-        gv->setVertexColor(path[0].getID(),"blue");
-        for (int i = 1; i < path.size(); i++) {
-            gv->setVertexColor(path[i].getID(),"green");
+    if (!path.empty()) {
+        gv->setVertexColor(path[0].getID(), "blue");
+        for (int i = 1; i<path.size(); i++) {
+            gv->setVertexColor(path[i].getID(), "green");
         }
-        gv->setVertexColor(path[path.size()-1].getID(),"cyan");
+        gv->setVertexColor(path[path.size()-1].getID(), "cyan");
     }
 
     getchar();
